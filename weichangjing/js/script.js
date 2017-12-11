214d0cf4c9dd9953fd71eac72cfcbbc08034cef6
zyshushu <2931219780@qq.com>
2017-12-11 14:33:17 +0800

    动画效果

--- weichangjing/js/script.js
+++ weichangjing/js/script.js
@@ -1,3 +1,16 @@
+//Swiper4.x
+var mySwiper = new Swiper ('.swiper-container', {
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
+})
+
 var swiper = new Swiper('.swiper-container', {
     //Slides的滑动方向，可设置水平(horizontal)或垂直(vertical)。
     direction: 'vertical',
