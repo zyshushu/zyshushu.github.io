f69d2f1b038f38f2d5d38d678f2952b8ff35bd25
zyshushu <2931219780@qq.com>
2017-12-11 15:18:30 +0800

    动画

--- weichangjing/js/script.js
+++ weichangjing/js/script.js
@@ -1,15 +1,4 @@
 //Swiper4.x
-var mySwiper = new Swiper ('.swiper-container', {
-    on:{
-        init: function(){
-            swiperAnimateCache(this); //隐藏动画元素
-            swiperAnimate(this); //初始化完成开始动画
-        },
-        slideChangeTransitionEnd: function(){
-            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
-        }
-    }
-})
 
 var swiper = new Swiper('.swiper-container', {
     //Slides的滑动方向，可设置水平(horizontal)或垂直(vertical)。
@@ -25,6 +14,15 @@ var swiper = new Swiper('.swiper-container', {
     navigation: {
         nextEl: '.swiper-button-next',
     },
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
 });
 
 
